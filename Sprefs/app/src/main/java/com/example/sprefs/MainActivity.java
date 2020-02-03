c2285297648856a97e0aad8c408995b76cbbee36
Nesar Ahmed <nesar13@hotmail.com>
2020-02-03 17:31:03 -0500

    Get Shared Preferences

--- Sprefs/app/src/main/java/com/example/sprefs/MainActivity.java
+++ Sprefs/app/src/main/java/com/example/sprefs/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.sprefs;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,8 +14,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private Button enterButton;
     private TextView showText;
-    private EditText editText;
-
+    private EditText enterMessage;
+    private final static String MESSAGE_ID = "messages";
 
 
     @Override
@@ -21,14 +23,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        enterButton=findViewById(R.id.enter_button);
-        editText=findViewById(R.id.name_textId);
-        showText=findViewById(R.id.show_text);
+        enterButton = findViewById(R.id.enter_button);
+        enterMessage = findViewById(R.id.name_textId);
+        showText = findViewById(R.id.show_text);
+
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = enterMessage.getText().toString().trim();
+
+                SharedPreferences sharedPreferences = getSharedPreferences(MESSAGE_ID, MODE_PRIVATE);
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                editor.putString("key", message);
 
 
+                editor.apply();
 
+            }
+        });
 
-
+        SharedPreferences getSharedData = getSharedPreferences(MESSAGE_ID, MODE_PRIVATE);
 
 
     }
